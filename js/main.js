2599f96a95cfddd744caa22499997158d952ff90
Margarita Mareeva <beffinda@gmail.com>
2022-06-14 00:00:28 +0300

    Выполнение первого дз
    Добавление стилей, упрощение функций

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
+    { id: 1, title: 'Notebook', price: 2000 },
+    { id: 2, title: 'Mouse', price: 20 },
+    { id: 3, title: 'Keyboard', price: 200 },
+    { id: 4, title: 'Gamepad', price: 50 },
 ];
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
-const renderProduct = (title, price) => {
+const renderProduct = (item) => {
     return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
+                <h3>${item.title}</h3>
+                <img src="https://picsum.photos/220?random=${item.id}" alt="image">
+                <p>${item.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
 };
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title,item.price));
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList;
+    document.querySelector('.products').innerHTML = (list.map(item => renderProduct(item))).join("");
 };
 
 renderPage(products);
