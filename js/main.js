67f2b15d1d6571035e2142aca1eff5c4ef528cd8
Margarita Mareeva <beffinda@gmail.com>
2022-06-14 18:36:00 +0300

    переписано с классами, добавлены классы корзины и товара в корзине

--- js/main.js
+++ js/main.js
@@ -1,21 +1,66 @@
-const products = [
-    { id: 1, title: 'Notebook', price: 2000 },
-    { id: 2, title: 'Mouse', price: 20 },
-    { id: 3, title: 'Keyboard', price: 200 },
-    { id: 4, title: 'Gamepad', price: 50 },
-];
-//Функция для формирования верстки каждого товара
-//Добавить в выводе изображение
-const renderProduct = (item) => {
-    return `<div class="product-item">
-                <h3>${item.title}</h3>
-                <img src="https://picsum.photos/220?random=${item.id}" alt="image">
-                <p>${item.price}</p>
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();//рекомендация, чтобы метод был вызван в текущем классе
+        this.render();//вывод товаров на страницу
+    }
+    _fetchProducts() {
+        this.goods = [
+            { id: 1, title: 'Notebook', price: 2000 },
+            { id: 2, title: 'Mouse', price: 20 },
+            { id: 3, title: 'Keyboard', price: 200 },
+            { id: 4, title: 'Gamepad', price: 50 },
+        ];
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+            block.insertAdjacentHTML("beforeend", item.render());
+
+        }
+    }
+}
+
+class ProductItem {
+    constructor(product) {
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = `https://picsum.photos/220?random=${this.id}`;
+    }
+    render() {
+        return `<div class="product-item">
+                <img src="${this.img}">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
-};
-const renderPage = list => {
-    document.querySelector('.products').innerHTML = (list.map(item => renderProduct(item))).join("");
-};
+    }
+}
 
-renderPage(products);
+let list = new ProductList();
+
+
+class Cart {
+    constructor() {
+        this.cartList = [];
+    }
+    addItem() { }
+    removeItem() { }
+    sumCart() { }
+    render() { }
+}
+class ItemCart {
+    /**
+     * 
+     * @param {*} count количество конкретного товара, по умолчанию 1
+     */
+    constructor(count = 1) {
+        this.count = count;
+    }
+    editCount() { }
+    render() { }
+}
