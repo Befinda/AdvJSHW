22676ea7bbd00f928ecfbf935fc7e3852fc43264
Margarita Mareeva <beffinda@gmail.com>
2022-06-25 19:14:52 +0300

    Добавление скрипта

---
+++ js/mainvue.js
@@ -0,0 +1,30 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        catalogUrl: '/catalogData.json',
+        products: [],
+        filtered: [],
+        imgCatalog: 'https://picsum.photos/220?random=',
+        userSearch: '',
+        show: false
+    },
+    methods: {
+        getJson(url) {
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+    },
+    mounted() {
+        this.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for (let el of data) {
+                    this.products.push(el);
+                }
+            });
+    }
+})
